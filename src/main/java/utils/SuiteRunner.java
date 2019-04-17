ce3c0228328ed1392b921167a1d6b982e2a18f8d
richard-u18 <rwyant@riseup.net>
2019-04-16 23:36:20 -0500

    added SuiteRunner class to kick off suite programatically, done correctly this time

--- src/main/java/utils/SuiteRunner.java
+++ src/main/java/utils/SuiteRunner.java
@@ -1,15 +1,21 @@
 package utils;
 
+import com.aventstack.extentreports.ExtentReports;
 import cucumber.api.cli.Main;
+import org.apache.maven.shared.invoker.*;
+import java.io.File;
+import java.util.Collections;
 
 public class SuiteRunner {
 
     public static void main(String[] args) {
 
+        //todo: make the directories compatible with Windows
         String [] argv = new String[]{
 //                "--help",""
                 "-g","","./src/main/resources/features"
-                ,"--plugin","html:target/this_is_my_report_yay_me.html" // i get an html report
+//                ,"--plugin","html:target/this_is_my_report_yay_me.html" // i get an html report
+                ,"--plugin","json:target/cucumber-report/cucumber.json" // json for cluecumber
         };
 
         ClassLoader contextClassLoader = Thread.currentThread().getContextClassLoader();
@@ -19,6 +25,20 @@ public class SuiteRunner {
             System.out.println("Cucumber CLI did an oopsie!");
             e.printStackTrace();
         }
+        
+
+//        InvocationRequest request = new DefaultInvocationRequest();
+//        String absPath = new File("").getAbsolutePath();
+//        request.setPomFile(new File(absPath + "/pom.xml"));
+//        request.setGoals(Collections.singletonList("cluecumber-report:reporting -Dparm=\"banana\""));
+//        Invoker invoker = new DefaultInvoker();
+//        invoker.setMavenHome(new File("/usr"));
+
+//        try {
+//            invoker.execute(request);
+//        } catch (MavenInvocationException e) {
+//            e.printStackTrace();
+//        }
     }
 }
 
---
+++ src/main/resources/features/rest-post-request.feature
@@ -0,0 +1,38 @@
+@rest @post @api
+	
+Feature: Create a new employee using POST in a REST web service
+
+#Background:
+#  Given user goes to html page
+
+Scenario: POST request with values defined in feature file
+  When send a "post" request to endpoint-resource "http://dummy.restapiexample.com/api/v1/create" with the below values
+    |name  |Starscream|
+    |salary|9000      |
+    |age   |50        |
+
+  Then the response comes back with the below values
+    |name  |Starscream|
+    |salary|9000      |
+    |age   |50        |
+    |id    |*         |
+
+#Scenario: POST request with values defined in JSONObject
+#  When send a "Post" request to endpoint-resource "http://dummy.restapiexample.com/api/v1/create" with the JSON "add-employee.json"
+#  Then the response comes back with the below values
+#    |name  |value   |
+#    |name  |Skywarp |
+#    |salary|9001    |
+#    |age   |51      |
+#    |id    |*       |
+#
+#Scenario: POST request defined in a SoapUI xml project file
+#  When send the request "Create" in SoapUI project "dummy.xml"
+#  Then the response comes back with the below values
+#    |name  |value   |
+#    |name  |Thundercracker|
+#    |salary|9002    |
+#    |age   |52      |
+#    |id    |*       |
+
+#Scenario: Test for an appropriate fail for a duplicate name
